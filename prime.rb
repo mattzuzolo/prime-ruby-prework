2c66109a2ed604f9d28089d008537cf8f90e838e
Matt Zuzolo <mattzuzolo@outlook.com>
2018-04-25 23:11:19 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -7,16 +7,12 @@ def prime? (num)
       counter = 2.0
         
       while counter < num
-        
-        if num % counter == 0
-           # puts "Num is divisible by #{counter}"
-            return false
-        end #end if
-        
-       # puts "counter = #{counter}"
-        counter += 1.0
-        
-      end # end loop
+          if num % counter == 0
+              return false
+          end
+       
+          counter += 1.0
+      end 
       
       return true
 
